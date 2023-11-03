949ea14d4d52ae701d597ba3df4ba39ff5aeb7df
asharif123 <awadsharif9@gmail.com>
2023-11-02 23:30:04 -0700

    completed reverseString program

--- reverse_string/Program.cs
+++ reverse_string/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace reverse_string
+﻿using System.Net.Sockets;
+
+//Create a program that asks the user for a string of input and simply returns it in reverse order.
+//For instance, they enter “Hello” and it returns “olleH”.
+
+//Added Difficulty: Check if the given string is a palindrome.
+//(a palindrome is a word, number, phrase, or other sequence of characters which reads the same
+//backward as forward, such as madam or racecar.)
+
+//rocket Tips: If you have problems understanding loops in general you could take a look at Module 3 regarding the debugger
+
+namespace reverse_string
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //ask user to enter a string
+            Console.WriteLine("Please enter a string:\n");
+            
+            //variable to store what user has inputted
+            string sentence = Console.ReadLine();
+
+            //variable to get the length of the sentence
+            int sentenceLength = sentence.Length;
+
+            //store the reverse of what user has inputted
+            string reverseSentence = "";
+
+            if (sentenceLength == 0) //if user does not enter any characters
+            {
+                Console.WriteLine("Please enter a sentence having at least 1 character!");
+            }
+            else if (sentenceLength == 1) //if user only enters 1 character, print that out
+            {
+                Console.WriteLine(sentence);
+                Console.WriteLine("\nThe reverse of {0} is {1}.", sentence, sentence);
+            }
+            else //if user enters 2 or more characters, store characters in reverse in reverse sentence
+            {
+                for (int i = sentenceLength-1; i >= 0; i--)
+                {
+                    reverseSentence += sentence[i];
+                }
+                //print the sentence in reverse
+                Console.WriteLine("\nThe reverse of {0} is {1}.", sentence, reverseSentence);
+                
+            }
+
+
+
+
+
+
+
         }
     }
 }
