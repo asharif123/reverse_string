1ab209da13799735340763e9c92ebb22f819fcfa
asharif123 <awadsharif9@gmail.com>
2023-11-08 21:19:45 -0800

    fixed code structure of last if statement

--- reverse_string/Program.cs
+++ reverse_string/Program.cs
@@ -38,9 +38,9 @@ namespace reverse_string
 
             //print the sentence in reverse, using .Trim() to remove whitespaces
             Console.WriteLine($"\nThe reverse of {sentence.Trim()} is {reverseSentence.Trim()}.");
+
             //to verify if it's a palindrome, see if sentence is equal to reverseSentence
             //if equal, it's palindrome else it's not a palindrome
-
             if (sentence.Trim().ToLower() == reverseSentence.Trim().ToLower())
             {
                 Console.WriteLine($"\n{sentence.Trim()} is a palindrome!");
